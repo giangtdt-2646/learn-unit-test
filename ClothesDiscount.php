4cd9a317b0ff0518fd71a46a110160623cb75525
Tran Giang <tran.dai.truong.giang@sun-asterisk.com>
2021-08-12 16:01:11 +0700

    using test coverage C0 for clothes discount test

--- ClothesDiscount.php
+++ ClothesDiscount.php
@@ -2,21 +2,20 @@
 
 function ClothesDiscountPercent($count, $hasShirt = false, $hasTie = false)
 {
-	if (!is_bool($hasShirt) || !is_bool($hasTie) || !is_int($count)) return "wrong argument type";
-	if ($count <= 0) return "There is 0 cloth";
-	
-	$quantityDiscount = 7;
-	$clothesTypeDiscount  = 5;
-	
-	if ($count >= 7) {
-		if ($hasShirt && $hasTie) {
-			return $quantityDiscount + $clothesTypeDiscount;
-		}
-		return $quantityDiscount;
-	}
+    if (is_bool($hasShirt) && is_bool($hasTie) && is_int($count) && $count > 0) {
+    $quantityDiscount = 7;
+    $clothesTypeDiscount  = 5;
 
-	if ($hasShirt && $hasTie) return $clothesTypeDiscount;
+    if ($count >= 7) {
+        if ($hasShirt && $hasTie) {
+            return $quantityDiscount + $clothesTypeDiscount;
+        }
+        return $quantityDiscount;
+    }
 
-	return 0;
+    if ($hasShirt && $hasTie) return $clothesTypeDiscount;
 
+    return 0;
+    }
+    return "wrong argument type";
 }
--- ClothesDiscountTest.php
+++ ClothesDiscountTest.php
@@ -1,18 +1,18 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+
 require 'ClothesDiscount.php';
 
 class ClothesDiscountTest extends TestCase
 {
     public function test_clothes_discount_percent()
     {
-        $this->assertEquals('wrong argument type', clothesDiscountPercent(1, 1,1));
-        $this->assertEquals('There is 0 cloth', clothesDiscountPercent(-1));
-        $this->assertEquals(12, clothesDiscountPercent(7, true,true));
-        $this->assertEquals(7, clothesDiscountPercent(7, true,false));
-        $this->assertEquals(7, clothesDiscountPercent(7, false,true));
-        $this->assertEquals(5, clothesDiscountPercent(6, true,true));
+        $this->assertEquals('wrong argument type', clothesDiscountPercent('1'));
+        $this->assertEquals(12, clothesDiscountPercent(7, true, true));
+        $this->assertEquals(7, clothesDiscountPercent(7, true, false));
+        $this->assertEquals(5, clothesDiscountPercent(6, true, true));
+        $this->assertEquals(0, clothesDiscountPercent(6));
 
     }
 }
